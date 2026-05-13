9a4fe2450ddafe40951da4605c7c476f770b7df1
nayaracalenzo <queiroz.14@hotmail.com>
2026-05-12 21:43:26 -0300

    página de login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,26 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import ListaProduto from "./ListaProduto.jsx";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [produtos, setProdutos] = useState([])
 
-  const menu = ["Promoções", "Horários", "Pedidos", "Login"]
+  const menu = [
+    {
+      desc: "Promoções",
+    },
+    {
+      desc: "Horários"
+    },
+    {
+      desc: "Pedidos",
+      link: "/carrinho"
+    },
+    {
+      desc: "Login",
+      link: "/login"
+    }]
   useEffect(() => {
     fetch("http://localhost:3000/produtos")
       .then(res => (res.json()))
@@ -21,48 +36,51 @@ const Header = () => {
 
   return (
     <>
-    <header className="bg-black px-20 py-15 flex justify-end relative mb-30">
-      <div className="rounded-full border-6 border-red-600 w-40 h-40 flex justify-center items-center absolute bg-black left-10">
-        <h1 className="text-white font-bold text-2xl px-4 text-center">SUSHI DIGITAL</h1>
-      </div>
-    {menu.map((item)=> (
-      <Dialog.Root>
-        <Dialog.Trigger className="w-32 text-white hover:underline underline-offset-8 cursor-pointer">
-        {item}
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 w-full h-full bg-black opacity-40" />
-        
-        <Dialog.Content className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full max-w-lg mx-auto px-4">
-          <div className="bg-white rounded-md shadow-lg">
-            <div className="flex items-center justify-between p-4 border-b">
-              <Dialog.Title className="text-lg font-medium text-gray-800 ">
-                PRODUTOS EM PROMOÇÃO
-              </Dialog.Title>
-              <Dialog.Close className="p-2 text-gray-400 rounded-md hover:bg-gray-100">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5 mx-auto"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </Dialog.Close>
-            </div>
-            <div>
-              <ListaProduto produtos={produtos} categoria="oferta" />
-            </div>
+      <header className="bg-black px-20 py-15 flex justify-end relative mb-30">
+        <div className="rounded-full border-6 border-red-600 w-40 h-40 flex justify-center items-center absolute bg-black left-10">
+          <h1 className="text-white font-bold text-2xl px-4 text-center">SUSHI DIGITAL</h1>
+        </div>
+        {menu.map((item, index) => (
+          <div key={index}>
+            {item.link ? (
+              <Link
+                to={item.link}
+                className="w-32 text-white hover:underline underline-offset-8 cursor-pointer block"
+              >
+                {item.desc}
+              </Link>
+            ) : (
+              <Dialog.Root>
+                <Dialog.Trigger className="w-32 text-white hover:underline underline-offset-8 cursor-pointer">
+                  {item.desc}
+                </Dialog.Trigger>
+
+                <Dialog.Portal>
+                  <Dialog.Overlay className="fixed inset-0 w-full h-full bg-black opacity-40" />
+
+                  <Dialog.Content className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full max-w-lg mx-auto px-4">
+                    <div className="bg-white rounded-md shadow-lg">
+                      <div className="flex items-center justify-between p-4 border-b">
+                        <Dialog.Title className="text-lg font-medium text-gray-800">
+                          PRODUTOS EM PROMOÇÃO
+                        </Dialog.Title>
+
+                        <Dialog.Close className="p-2 text-gray-400 rounded-md hover:bg-gray-100">
+                          X
+                        </Dialog.Close>
+                      </div>
+
+                      <div>
+                        <ListaProduto produtos={produtos} categoria="oferta" />
+                      </div>
+                    </div>
+                  </Dialog.Content>
+                </Dialog.Portal>
+              </Dialog.Root>
+            )}
           </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-    ))}
-   </header>
+        ))}
+      </header>
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,50 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  async function handleLogin(e) {
+    e.preventDefault()
+    console.log(email, password)
+    setLoading(true)
+    try {
+      const usuarios = await fetch("http://localhost:3000/usuarios")
+      const res = await usuarios.json()
+      const sucesso = res.find((usuario) => (
+        usuario.email === email && usuario.password === password
+      ))
+      console.log(res)
+      if (sucesso) {
+        navigate("/")
+      }
+    } catch (error) {
+      console.error("Erro na requisição", error)
+    }
+  }
+
   return (
     <>
-      <h1>Página Login</h1>
-    </>
+    { loading ? <p>Carregando...</p> : (
+    <div className="bg-gray-950 h-screen flex justify-center items-center">
+      <form className="bg-gray-900/95 w-1/2 flex justify-around items-center py-10 rounded-2xl" onSubmit={handleLogin}>
+        <div className="rounded-full border-6 border-red-600 w-40 h-40 flex justify-center items-center bg-black left-10">
+          <h1 className="text-white font-bold text-2xl px-4 text-center">SUSHI DIGITAL</h1>
+        </div>
+        <div className="w-1/2 grid gap-2 justify-items-end">
+          <input className="w-full bg-white/45 p-1.5 rounded" type="email" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input className="w-full bg-white/45 p-1.5 rounded" type="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button className="bg-red-700 rounded px-4 py-1.5 w-1/3" type="submit">Entrar</button>
+        </div>
+      </form>
+    </div> )}
+  </>
   )
-
 }
 
 export default Login
